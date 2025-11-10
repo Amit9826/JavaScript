9f0755d355ddd462416a51dd812806eb0da9966e
Amit9826 <suryawanshiamit492@gmail.com>
2025-11-10 16:28:56 +0530

    update

---
+++ String.js
@@ -0,0 +1,36 @@
+// let name = "Amit Suryawanshi";
+// console.log(name[5])
+
+
+// let city ="BHOPAL";
+// let newname = name.toUpperCase()
+// console.log(newname)
+
+// console.log(city.toLowerCase())
+
+// let nam = " Amit "
+// console.log(nam.trim())
+
+
+// let n = " rahul";
+// console.log(n.trimStart())
+
+// let na = "Rohit ";
+// console.log(na.trimStart())
+
+
+// include , match , charAt , concat , length , index of , replace =>  
+
+
+
+    let faculty = "By xyz"
+    let st = "this is \njs class" + " " +faculty
+    // console.log(st)
+
+    /// templete literals ` `
+    let st1 = `this is javaScript 
+    class ${faculty}`
+
+    console.log(st1)
+
+
