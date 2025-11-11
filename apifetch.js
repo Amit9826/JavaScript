50366c26e1c73855ac8660f05d3c0a7d26a56d1d
Amit9826 <suryawanshiamit492@gmail.com>
2025-11-11 16:33:01 +0530

    update

---
+++ apifetch.js
@@ -0,0 +1,22 @@
+// API is any method use => fatch function
+
+ async function access( ) {
+
+    let data = await fetch('https://jsonplaceholder.typicode.com/posts')
+
+    let res = await data.json()
+    
+    // console.log(res);
+
+    let result = res.map((e)=>` 
+    <tr>
+    <td> ${e.userId} </td>
+     <td> ${e.id} </td>
+      <td> ${e.title} </td>
+      </tr>
+    `
+ ).join("")
+
+ document.querySelector('#showdata').innerHTML = result
+
+ access()
